f0680f8cf8a479ec73ff9730447def19763beb0d
EastonArcher <ea02archer@gmail.com>
2024-01-03 17:15:00 -0600

    Automated PDF Merger

---
+++ PDFMerger/pdfMerger.py
@@ -0,0 +1,10 @@
+import PyPDF2
+import sys
+import os
+
+merger = PyPDF2.PdfFileMerger()
+
+for file in os.listdir(os.curdir):
+    if file.endswith(".pdf"):
+        merger.append(file)
+    merger.write("Combined.pdf")

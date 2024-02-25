8143d28fab478382b1ca7c6a647b01c73328d33f
Easton Archer <136001956+EastonArcher@users.noreply.github.com>
2024-02-25 13:34:27 -0600

    Update pdfMerger.py

--- PDFMerger/pdfMerger.py
+++ PDFMerger/pdfMerger.py
@@ -11,4 +11,6 @@ for file in os.listdir(os.curdir):
     if file.endswith(".pdf"):
         # If the file is a PDF, append it to the merger object
         merger.append(file)
-    merger.write("Combined.pdf")
+        
+# Write the merged PDF to a file named "Combined.pdf"
+merger.write("Combined.pdf")

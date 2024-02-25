630d8622e624eb15dbd3d8c35306b0df6f3422b2
Easton Archer <136001956+EastonArcher@users.noreply.github.com>
2024-02-25 13:33:40 -0600

    Update pdfMerger.py

--- PDFMerger/pdfMerger.py
+++ PDFMerger/pdfMerger.py
@@ -5,6 +5,7 @@ import os
 # Initialize a PdfFileMerger object from PyPDF2 library
 merger = PyPDF2.PdfFileMerger()
 
+# Iterate through all files in the current directory
 for file in os.listdir(os.curdir):
     if file.endswith(".pdf"):
         merger.append(file)
